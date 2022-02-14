256cafe2742a6076f9109ab6c2e1592c5c9fcca3
AlisileAnusri <alisile.anusri@gmail.com>
2022-02-14 17:50:57 +0000

    Final

---
+++ final_project/machinetranslator/tests/tests.py
@@ -0,0 +1,16 @@
+from translator import englishtofrench,frenchtoenglish
+import unittest
+
+class testenfr(unittest.TestCase):
+    def test_eng_fr(self):
+        self.assertEqual(englishtofrench('Hello'),'Bonjour')
+        self.assertEqual(englishtofrench('My Love'),'Mon amour')
+        self.assertEqual(englishtofrench(''),'Empty')
+
+class testfren(unittest.TestCase):
+    def test_fr_eng(self):
+        self.assertEqual(frenchtoenglish('Bonjour'),'Hello')
+        self.assertEqual(frenchtoenglish('Mon amour'),'My Love')
+        self.assertEqual(frenchtoenglish(''),'Empty')
+
+unittest.main()
