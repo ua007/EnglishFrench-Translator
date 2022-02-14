87f2b89112e2bf35c38fe18ebe8cc1c15d1b59e5
AlisileAnusri <alisile.anusri@gmail.com>
2022-02-14 18:52:57 +0000

    Final

--- final_project/machinetranslator/tests.py
+++ final_project/machinetranslator/tests.py
@@ -6,11 +6,13 @@ class testenfr(unittest.TestCase):
         self.assertEqual(englishtofrench('Hello'),'Bonjour')
         self.assertEqual(englishtofrench('My Love'),'Mon amour')
         self.assertEqual(englishtofrench(''),'Empty')
+        self.assertNotEqual(englishtofrench('School'),'Lecole')
 
 class testfren(unittest.TestCase):
     def test_fr_eng(self):
         self.assertEqual(frenchtoenglish('Bonjour'),'Hello')
         self.assertEqual(frenchtoenglish('Mon amour'),'My Love')
         self.assertEqual(frenchtoenglish(''),'Empty')
+        self.assertNotEqual(frenchtoenglish('Fleur'),'Flow')
 
 unittest.main()
