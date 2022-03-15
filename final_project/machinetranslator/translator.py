f203d581873a9f93ce1e2cc672e577f1eaaabae8
AlisileAnusri <alisile.anusri@gmail.com>
2022-03-15 09:24:20 +0000

    Final

--- final_project/machinetranslator/translator.py
+++ final_project/machinetranslator/translator.py
@@ -2,10 +2,13 @@
 import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from dotenv import load_dotenv
+#import pandas
+#from pandas import json_normalize
 
+load_dotenv()
 apikey=os.environ['apikey']
 url=os.environ['url']
-
 
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
